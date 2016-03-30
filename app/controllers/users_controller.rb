f45a4f5b36450698fe698bad7ea4fdc5406b3e1d
frick0412 <yuu72ki@yahoo.co.jp>
2016-03-30 14:20:05 +0000

    user plofile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
-  def show # 追加
+  
+  def show 
    @user = User.find(params[:id])
   end
   
@@ -13,18 +16,44 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+      redirect_to @user 
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to @user
+    else
+      #保存に失敗した場合は編集画面に戻す
+      render 'edit'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :location, :email,
+                                 :password, :password_confirmation)
   end
+  # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+      # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
+    end
 end
-
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,11 @@ ActiveRecord::Schema.define(version: 20160328142600) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "location"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
