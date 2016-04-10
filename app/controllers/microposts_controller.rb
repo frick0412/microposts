3309af2e0859ffb4cfea10d547fd71d7129e14be
frick0412 <yuu72ki@yahoo.co.jp>
2016-04-10 14:18:25 +0000

    add home view's timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,15 +1,17 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create]
 
- def create
+  def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc) 
       render 'static_pages/home'
     end
   end
+
  
     
   def destroy
@@ -18,7 +20,7 @@ class MicropostsController < ApplicationController
     @micropost.destroy
     flash[:success] = "Micropost deleted"
     redirect_to request.referrer || root_url
-    end
+  end
 
   
   private
